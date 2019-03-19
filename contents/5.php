b97f4612c925950bcfb125a6d7b85937c49e7176
tibike8906 <47151702+tibike8906@users.noreply.github.com>
2019-03-19 14:59:01 +0100

    Youtube video és a beágyazott videó FTP-ről

--- contents/5.php
+++ contents/5.php
@@ -2,5 +2,15 @@
 <html>
 <head>
 </head>
+<body>
+	<b>Youtube</b><br>
+	<iframe width="420" height="315"
+		src="https://www.youtube.com/embed/kqx1qgdVMIk">
+	</iframe><br>
+	<b>Beágyazott FTP-ről</b><br>
+	<video poster="images/cimergif.gif" id="player" playsinline controls height="315">
+		<source src="videos/s1.mp4" type="video/mp4">		
+	</video>
+	
 </body>
 </html>
