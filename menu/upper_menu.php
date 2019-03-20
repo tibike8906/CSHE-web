c6b4d6faae13db7d74be488a3aed6ad57cff5163
tibike8906 <47151702+tibike8906@users.noreply.github.com>
2019-03-20 08:29:03 +0100

    bugfix

--- menu/upper_menu.php
+++ menu/upper_menu.php
@@ -1,14 +1,14 @@
 <?php
   $upper_menu = Array(
-    1 => Array("szoveg" => "Nyitólap", "cim" => "Nyitólap"),
-    2 => Array("szoveg" => "Rólunk", "cim" => "Rólunk"),
-    3 => Array("szoveg" => "Programok", "cim" => "Programok"),
-	4 => Array("szoveg" => "Galéria", "cim" => "Galéria"),
-	5 => Array("szoveg" => "Videók", "cim" => "Videók"),
-	6 => Array("szoveg" => "Email", "cim" => "Email küldés az üzemeltetőnek!"),
-	7 => Array("szoveg" => "Bejelentkezés", "cim" => "Bejelentkezés"),
-	8 => Array("szoveg" => "Kijelentkezés", "cim" => "Kijelentkezés"),
-	9 => Array("szoveg" => "Regisztráció", "cim" => "Regisztráció")
+    1 => Array("pid" => "?pid=1", "szoveg" => "Nyitólap", "cim" => "Nyitólap"),
+    2 => Array("pid" => "?pid=2", "szoveg" => "Rólunk", "cim" => "Rólunk"),
+    3 => Array("pid" => "?pid=3", "szoveg" => "Programok", "cim" => "Programok"),
+	4 => Array("pid" => "?pid=4", "szoveg" => "Galéria", "cim" => "Galéria"),
+	5 => Array("pid" => "?pid=5", "szoveg" => "Videók", "cim" => "Videók"),
+	6 => Array("pid" => "?pid=6", "szoveg" => "Email", "cim" => "Email küldés az üzemeltetőnek!"),
+	7 => Array("pid" => "?pid=7", "szoveg" => "Bejelentkezés", "cim" => "Bejelentkezés"),
+	8 => Array("pid" => "?pid=8", "szoveg" => "Kijelentkezés", "cim" => "Kijelentkezés"),
+	9 => Array("pid" => "?pid=9", "szoveg" => "Regisztráció", "cim" => "Regisztráció")
   );
   if(isset($_SESSION["valid"])){
 	$valid = "true";	  
@@ -16,24 +16,19 @@
 	$valid = "false";	
   }	
   foreach ($upper_menu as $id => $leiro) {
-    echo "<li";
+	echo "<li";
+	if ( $valid == "false" && $id == 8){
+		echo " class=\"disabledLink\"";
+		$leiro["pid"] = "#";
+	}    
+	if ( $valid == "true" && ($id == 7 || $id == 9)){
+		echo " class=\"disabledLink\"";
+		$leiro["pid"] = "#";
+	}
     if ($id == $aktualis_oldal)
       echo " class=\"current_page_item\"";
-	if ( $valid == "false"){
-		if($id == 8){
-			echo " class=\"disabledLink\"";
-			echo "><a title=\"".$leiro["cim"]."\" href=\"#\">".$leiro["szoveg"]."</a></li>\n\t";
-		} else {
-			echo "><a title=\"".$leiro["cim"]."\" href=\"?pid=$id\">".$leiro["szoveg"]."</a></li>\n\t";
-		}		
-	} else {
-		if($id == 7 || $id == 9){
-			echo " class=\"disabledLink\"";
-			echo "><a title=\"".$leiro["cim"]."\" href=\"#\">".$leiro["szoveg"]."</a></li>\n\t";
-		} else {
-			echo "><a title=\"".$leiro["cim"]."\" href=\"?pid=$id\">".$leiro["szoveg"]."</a></li>\n\t";
-		}		
-	}
+  
+	echo "><a title=\"".$leiro["cim"]."\" href=\"". $leiro["pid"] ."\">".$leiro["szoveg"]."</a></li>\n\t";
   }
 
 ?>
